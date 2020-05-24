2d77f904a7009a4d9c5b117292f0101f119f8cc2
Diomede Mazzone <diomede.mazzone@gmail.com>
2020-05-25 00:46:43 +0200

    primi test per le immagini

--- nomi_file.py
+++ nomi_file.py
@@ -1,9 +1,23 @@
-#from picamera import PiCamera
-import time
+from pathlib import Path
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw 
 
-#camera.start_preview()
-while(1):
-	time.sleep(1)
-	data = '{:>5.0f}'.format(time.monotonic())
-	f = open('file/%s.txt'%data, 'w')
-	#    camera.capture('/home/pi/image%s.jpg' % i)
+data_folder = Path("image/")
+
+print (data_folder)
+
+file_to_open = data_folder / "stills.txt"
+
+with open(file_to_open) as f:
+	
+	print(f.read())
+	
+	img = Image.open(read_data)
+	
+	draw = ImageDraw.Draw(img)
+# font = ImageFont.truetype(<font-file>, <font-size>)
+	font = ImageFont.truetype("sans-serif.ttf", 16)
+# draw.text((x, y),"Sample Text",(r,g,b))
+	draw.text((0, 0),"Sample Text",(255,255,255),font=font)
+	img.save(data_folder + 'out' + read_data)
